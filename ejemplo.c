6dcdecc051468be175442aaa7a6b7cee024db249
René Pérez <rene.perez1976@gmail.com>
2019-04-16 15:42:17 -0300

    mi primer commit

---
+++ ejemplo.c
@@ -0,0 +1,8 @@
+#include
+
+int main(){
+	printf("Hello World desde Git");
+	printf("\nEste es mi primer programa aplicando GIT");
+	
+	return 0;
+}
---
+++ ejemplo2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("Otro programa de prueba con C y usando Git.\n");
+	printf("Esperemos que funcione.\n");
+
+	return 0;
+}
